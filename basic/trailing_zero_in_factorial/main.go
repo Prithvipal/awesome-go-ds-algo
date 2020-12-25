6437550d0e2f4142a2af6d986ffd1d405a827895
Prithvipal Singh <prithvirathore99@gmail.com>
2020-12-25 21:48:19 +0530

    trailing zero in factorial

---
+++ basic/trailing_zero_in_factorial/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 259
+	count1 := getTrailingZeroInFact(n)
+	fmt.Println(count1)
+	count2 := countTrailignZeros(n)
+	fmt.Println(count2)
+
+}
+
+// using while loop
+func getTrailingZeroInFact(n int) int {
+	d := 5
+	count := 0
+	for n/d != 0 {
+		count = count + (n / d)
+		d = d * 5
+	}
+	return count
+}
+
+func countTrailignZeros(n int) int {
+	count := 0
+	for i := 5; i < n; i = i * 5 {
+		count = count + (n / i)
+	}
+	return count
+}
