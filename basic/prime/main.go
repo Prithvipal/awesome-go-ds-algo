3d5d02d2ab28030dc8cf05440c329d8173f1b943
Prithvipal Singh <prithvirathore99@gmail.com>
2020-12-26 11:47:10 +0530

    more efficient prime number

---
+++ basic/prime/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	prime := isPrime(101)
+	fmt.Println(prime)
+}
+
+func isPrime(n int) bool {
+	if n == 1 {
+		return false
+	}
+	if n == 2 || n == 3 {
+		return true
+	}
+	if n%2 == 0 || n%3 == 0 {
+		return false
+	}
+	for i := 5; i*i <= n; i = i + 6 {
+		if n%i == 0 || n%(i+2) == 0 {
+			return false
+		}
+	}
+	return true
+}
