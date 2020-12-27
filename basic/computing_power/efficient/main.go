715ecec01bb0ecc6ed3a38263d77d12c5c9ff794
Prithvipal Singh <prithvirathore99@gmail.com>
2020-12-27 17:35:24 +0530

    power of number with efficient solution

--- basic/computing_power/efficient/main.go
+++ basic/computing_power/efficient/main.go
@@ -1,7 +1,21 @@
 package main
 
-
+import "fmt"
 
 func main() {
+	p := power(4, 3)
+	fmt.Println(p)
+}
+
+func power(x, n int) int {
+	if n == 0 {
+		return 1
+	}
+	temp := power(x, n/2)
+	temp = temp * temp
+	if n%2 == 0 {
+		return temp
+	}
+	return temp * x
 
 }
